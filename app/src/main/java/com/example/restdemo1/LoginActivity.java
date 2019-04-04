5fca8875a387fda15f525f7b5496fb6f6f6b7d1f
Aqib - Android <mirzaaqibbeg@gmail.com>
2019-04-04 09:23:02 +0400

    Initial commit for RestoDxb Pocs

---
+++ app/src/main/java/com/example/restdemo1/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.restdemo1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+
+
+    @OnClick(R.id.btn_login)
+    void onCreateAccountClickn() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @OnClick(R.id.btn_create_account)
+    void onCreateAccountClick() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
